0064369a4c55a03f3c53fb40124918bb3d6e7240
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2019-07-30 12:01:44 +0200

    ACPI: PM: s2idle: Eliminate acpi_sleep_no_ec_events()
    Change acpi_ec_suspend() to use pm_suspend_no_platform() instead of
    acpi_sleep_no_ec_events(), which allows the latter to be eliminated
    along with the s2idle_in_progress variable which is only used by it.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/sleep.c
+++ drivers/acpi/sleep.c
@@ -696,7 +696,6 @@ static const struct platform_suspend_ops acpi_suspend_ops_old = {
 	.recover = acpi_pm_finish,
 };
 
-static bool s2idle_in_progress;
 static bool s2idle_wakeup;
 
 /*
@@ -950,7 +949,6 @@ static struct acpi_scan_handler lps0_handler = {
 static int acpi_s2idle_begin(void)
 {
 	acpi_scan_lock_acquire();
-	s2idle_in_progress = true;
 	return 0;
 }
 
@@ -1042,7 +1040,6 @@ static void acpi_s2idle_restore(void)
 
 static void acpi_s2idle_end(void)
 {
-	s2idle_in_progress = false;
 	acpi_scan_lock_release();
 }
 
@@ -1072,7 +1069,6 @@ static void acpi_sleep_suspend_setup(void)
 }
 
 #else /* !CONFIG_SUSPEND */
-#define s2idle_in_progress	(false)
 #define s2idle_wakeup		(false)
 #define lps0_device_handle	(NULL)
 static inline void acpi_sleep_suspend_setup(void) {}
@@ -1081,11 +1077,6 @@ static inline void acpi_sleep_suspend_setup(void) {}
 bool acpi_s2idle_wakeup(void)
 {
 	return s2idle_wakeup;
-}
-
-bool acpi_sleep_no_ec_events(void)
-{
-	return !s2idle_in_progress;
 }
 
 #ifdef CONFIG_PM_SLEEP
